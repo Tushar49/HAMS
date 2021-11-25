419da76aaef9f2c8365d555534caf6d530dc89c7
Tushar Agarwal <76201310+Tushar49@users.noreply.github.com>
2021-11-26 01:10:21 +0530

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Screens/loginPortal.dart';
 import 'package:flutter_application_1/Screens/loginPortal2.dart';
-
-
 import 'Screens/homepage1.dart';
 
 void main() {
